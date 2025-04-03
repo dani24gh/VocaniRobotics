957d6a1beff663f1870bfc7147b3bf3a2117cd56
dani24gh <19047@portalcedi.edu.mx>
2025-04-03 08:52:11 -0600

    authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjKilGP5th8TRr1wlVUnoTJFIC-7H38iU",
+  authDomain: "vocanirobotics.firebaseapp.com",
+  projectId: "vocanirobotics",
+  storageBucket: "vocanirobotics.firebasestorage.app",
+  messagingSenderId: "789542436260",
+  appId: "1:789542436260:web:3faf885b62a5a2cb3d9531"  },
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from './environments/environment';
+
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+if (environment.production) {
+  enableProdMode();
+}
+
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideIonicAngular(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
 });
