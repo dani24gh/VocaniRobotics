5431fd5bbbec921644d4ebd961f1fc8ce26c5f41
dani24gh <19047@portalcedi.edu.mx>
2025-04-03 08:43:26 -0600

    PC

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VocaniRobotics',
+  webDir: 'www'
+};
+
+export default config;
