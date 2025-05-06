691c515f5ff43e188b135018867dd9fa7c320501
dani24gh <19047@portalcedi.edu.mx>
2025-05-06 08:31:54 -0600

    papu home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,19 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-=======
-import { IonicModule } from '@ionic/angular';  
-import { FormsModule } from '@angular/forms';  
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
->>>>>>> 16c97e797d77247e94d6c0007c33cbdba63b38fc
 
 @Component({
   standalone: true,
   selector: 'app-home',
-<<<<<<< HEAD
-  imports: [CommonModule, IonicModule, RouterModule],
+  imports: [CommonModule, IonicModule, RouterModule, FormsModule],
   templateUrl: './home.page.html',
-=======
-  standalone: true, 
-  imports: [IonicModule, FormsModule],  
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
->>>>>>> 16c97e797d77247e94d6c0007c33cbdba63b38fc
+  styleUrls: ['./home.page.scss']
 })
-
-
 export class HomePage {
-<<<<<<< HEAD
   items = [
     { name: 'Arduino Mega2560 R3', quantity: 3, description: 'Microcontrolador potente para proyectos complejos.', image: 'assets/mega2560.jpg' },
     { name: 'Módulo SiM- mod. SINB00L', quantity: 5, description: 'Permite comunicación GSM/GPRS.', image: 'assets/sim-module.jpg' },
@@ -131,122 +116,17 @@ export class HomePage {
     { name: 'Filamento para impresión 3D', quantity: 3, description: 'Filamento para impresora 3D.', image: 'assets/3d-filament.jpg' }
   ];
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   goToDetail(item: any) {
     history.pushState(null, '', '/item-detail');
     sessionStorage.setItem('selectedItem', JSON.stringify(item));
     location.href = '/item-detail';
   }
-=======
-
-  constructor(private authService: AuthService, private router:Router){}
-
-  quantityMega2560: number = 10;
-  quantitySimModule: number = 0;
-  quantityCapacitors: number = 0;
-  quantityMAX7219: number = 0;
-  quantityL2930: number = 0;
-  quantitySN74HCS95: number = 0;
-  quantityDiodes: number = 0;
-  quantityDisplays: number = 0;
-  quantityESP32: number = 0;
-  quantityGalga: number = 0;
-  quantityHelices: number = 0;
-  quantityLEDs: number = 0;
-  quantityLedMatrix: number = 0;
-  quantityAdapter: number = 0;
-  quantityPCA9685: number = 0;
-  quantityRFID: number = 0;
-  quantityRelay: number = 0;
-  quantitySensorLine: number = 0;
-  quantityVoiceSensor: number = 0;
-  quantityRTC: number = 0;
-  quantityShield: number = 0;
-  quantityProtoshield: number = 0;
-  quantityPushButton: number = 0;
-  quantityResistances: number = 0;
-  quantityColorSensor: number = 0;
-  quantityCapacitiveHumidity: number = 0;
-  quantityTH11: number = 0;
-  quantityFlexSensors: number = 0;
-  quantityWaterLevel: number = 0;
-  quantityPIR: number = 0;
-  quantityUltrasonic: number = 0;
-  quantityServo: number = 0;
-  quantityThermistors: number = 0;
-  quantityTecladoMatricial = 0;
-  quantityTransistorBC547 = 0;
-  quantityTransistorBC557 = 0;
-  quantityTransistorPN2222 = 0;
-  quantityTarjetaExtensora = 0;
-  quantityServomotorESOBA = 0;
-  quantitySoilHumiditySensor = 0;
-  quantityConectores = 0;
-  quantityRainSensor = 0;
-  quantityTemperatureSensor = 0;
-  quantityWaferTerminalBlock = 0;
-  quantityBoton = 0;
-  quantityMotorreductores = 0;
-  quantityControlParoto = 0;
-  quantityLeds = 0;
-  quantityRuedas = 0;
-  quantityCautinesBase = 0;
-  quantityCutterTrupper = 0;
-  quantityDesoldadorSuccion = 0;
-  quantityFlexometroUltra = 0;
-  quantityKitDesarmadores27 = 0;
-  quantityKitDesarmadoresTrupper = 0;
-  quantityKitLimasJoyero = 0;
-  quantityMiniMartilloPretul = 0;
-  quantityMiniMototool = 0;
-  quantityMultimetro = 0;
-  quantityPieRey = 0;
-  quantityPinzasCorte = 0;
-  quantityPinzasPlanas = 0;
-  quantityPistolasSilicon = 0;
-  quantitySeguetaSierra = 0;
-  quantitySoportesEscuadra = 0;
-  quantityPelacables = 0;
-  quantityBrochaPintura = 0;
-  quantityCablesVarios = 0;
-  quantityCinchoMini = 0;
-  quantityLentesSeguridad = 0;
-  quantityLimpiadorWD40 = 0;
-  quantityPapelLijaMadera = 0;
-  quantityCinchoChico = 0;
-  quantityCinchoMediano = 0;
-  quantityPatoMadera = 0;
-  quantityCinchoGrande = 0;
-  quantityPegamentoBlanco = 0;
-  quantityCintaAdhesivaDiurex = 0;
-  quantityPegamentoKolaLoka = 0;
-  quantityCintaAislante = 0;
-  quantityPegamentoPrittStick = 0;
-  quantityCintaCanela = 0;
-  quantityPilasAlcalinasAA = 0;
-  quantityCintaDobleCara = 0;
-  quantityPilasRecargables9V = 0;
-  quantityCintaMaskingTape = 0;
-  quantityPilasRecargablesAA = 0;
-  quantityCintaParaDuctos = 0;
-  quantityPinturaAcrilica = 0;
-  quantityColasDePato = 0;
-  quantitySiliconBarras = 0;
-  quantitySiliconFrio = 0;
-  quantityLigas10Chica = 0;
-  quantityTijeras = 0;
-  quantityLigas18Grande = 0;
-  quantityVarios = 0;
-  quantityLaminasAcrylico = 0;
-  quantityVelcroCuadros = 0;
-  quantityFilamento3D = 0;
 
   logout() {
-    this.authService.logout(); 
+    this.authService.logout();
     this.router.navigate(['/login']);
     console.log('Sesión cerrada desde AuthService');
   }
-
->>>>>>> 16c97e797d77247e94d6c0007c33cbdba63b38fc
 }
